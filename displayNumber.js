1393b509b4b2e1baff80270b96da3b9979b3f9a1
Moustaph <103828108+Moustaph0@users.noreply.github.com>
2023-02-14 17:34:34 +0100

    Update displayNumber.js

--- displayNumber.js
+++ displayNumber.js
@@ -1,4 +1,4 @@
-tab [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+tab = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 function displayNumber(tableau) {
     for(let i = 0; i < tableau.length; i++) {
         console.log(tableau[i]);
